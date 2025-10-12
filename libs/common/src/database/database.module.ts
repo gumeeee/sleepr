ab66b4dbf47f91b860565e8d19e08b0cf98c483d
gumeeee <gaxco123@gmail.com>
2025-10-11 23:59:27 -0300

    feat(config): integrate global configuration with Joi validation and update modules to use ConfigService

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [
-    ConfigModule,
     MongooseModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGODB_URI'),
