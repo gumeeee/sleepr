efb055a21741f94f69282ac74b067c01b62689f5
gumeeee <gaxco123@gmail.com>
2025-10-24 18:02:57 -0300

    refactor(payments): move Stripe initialization to constructor for better clarity

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -5,14 +5,13 @@ import { CreateChargeDto } from '../dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  private readonly stripe = new Stripe(
-    this.configService.get('STRIPE_SECRET_KEY')!,
-    {
-      apiVersion: '2025-09-30.clover',
-    },
-  );
+  private readonly stripe: Stripe;
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {
+    this.stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY')!, {
+      apiVersion: '2025-09-30.clover',
+    });
+  }
 
   async createCharge({ card, amount }: CreateChargeDto) {
     const paymentMethod = await this.stripe.paymentMethods.create({
