1c886984fcaccd462350db10e2b138d9515aafa4
gumeeee <gaxco123@gmail.com>
2025-10-25 19:35:04 -0300

    feat(notifications): implement notification by gmail

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,10 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import nodemailer from 'nodemailer';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NotificationsService {
-  // eslint-disable-next-line @typescript-eslint/require-await
-  async notifyEmail({ email }: NotifyEmailDto) {
-    console.log(email);
+  private readonly transporter: nodemailer.Transporter;
+  constructor(private readonly configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: this.configService.get<string>('SMTP_USER'),
+        clientId: this.configService.get<string>('GOOGLE_OAUTH_CLIENT_ID'),
+        clientSecret: this.configService.get<string>(
+          'GOOGLE_OAUTH_CLIENT_SECRET',
+        ),
+        refreshToken: this.configService.get<string>(
+          'GOOGLE_OAUTH_REFRESH_TOKEN',
+        ),
+      },
+    });
+  }
+
+  async notifyEmail({ email, text }: NotifyEmailDto) {
+    await this.transporter.sendMail({
+      from: this.configService.get<string>('SMTP_USER'),
+      to: email,
+      subject: 'Sleepr Notification',
+      text,
+    });
   }
 }
