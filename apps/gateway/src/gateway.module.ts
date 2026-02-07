3c9a9d6f4cf78de53673414766834f3e99719f96
gumeeee <gaxco123@gmail.com>
2026-02-07 18:14:04 -0300

    feat: implement authentication context and app setup for GraphQL gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,15 +1,20 @@
-import { LoggerModule } from '@app/common';
+import { AUTH_SERVICE, LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { IntrospectAndCompose } from '@apollo/gateway';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { authContext } from './auth.context';
 
 @Module({
   imports: [
     GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
       useFactory: (configService: ConfigService) => ({
+        server: {
+          context: authContext,
+        },
         gateway: {
           supergraphSdl: new IntrospectAndCompose({
             subgraphs: [
@@ -19,10 +24,34 @@ import { IntrospectAndCompose } from '@apollo/gateway';
               },
             ],
           }),
+          buildService({ url }) {
+            return new RemoteGraphQLDataSource({
+              url,
+              willSendRequest({ request, context }) {
+                request.http?.headers.set(
+                  'user',
+                  context.user ? JSON.stringify(context.user) : '',
+                );
+              },
+            });
+          },
         },
       }),
       inject: [ConfigService],
     }),
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.getOrThrow<string>('AUTH_SERVICE_HOST'),
+            port: configService.getOrThrow<number>('AUTH_SERVICE_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { UserDocument } from '@app/common';
 
-const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {
-  return context.switchToHttp().getRequest<{ user: UserDocument }>().user;
+const getCurrentUserByContext = (
+  context: ExecutionContext,
+): UserDocument | undefined => {
+  if (context.getType() === 'http') {
+    return context.switchToHttp().getRequest<{ user: UserDocument }>().user;
+  }
+  const user = context.getArgs()[2]?.req.headers?.user;
+
+  if (user) {
+    return JSON.parse(user) as UserDocument;
+  }
 };
 
 export const CurrentUser = createParamDecorator(
