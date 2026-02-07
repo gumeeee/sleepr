b996a99c5c34dc960378d3b6211034570eb3ee68
gumeeee <gaxco123@gmail.com>
2026-02-07 15:16:43 -0300

    Refactor code structure for improved readability and maintainability

---
+++ apps/gateway/src/gateway.module.ts
@@ -0,0 +1,34 @@
+import { LoggerModule } from '@app/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
+
+@Module({
+  imports: [
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      useFactory: (configService: ConfigService) => ({
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              {
+                name: 'reservations',
+                url: configService.getOrThrow('RESERVATIONS_GRAPHQL_URL'),
+              },
+            ],
+          }),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    LoggerModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class GatewayModule {}
